6206b8c83c351c16cf84a4f89a686d886aef9cab
sadhana1058 <sadhana1058@gmail.com>
2025-07-09 19:54:22 -0400

    navbar

--- personal-portfolio-sadhu/src/components/NavBar.js
+++ personal-portfolio-sadhu/src/components/NavBar.js
@@ -1,21 +1,59 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import {Navbar,Container,Nav} from 'react-bootstrap';
+import ss_logo from '../assets/Images/ss_logo.jpeg';
+import navIcon1 from '../assets/Images/nav-icon1.svg';
+import navIcon2 from '../assets/Images/nav-icon2.svg';
+import navIcon3 from '../assets/Images/nav-icon3.svg';
+import { useEffect,useState } from 'react';
+// import { HashLink } from 'react-router-hash-link';
 
 const NavBar = () => {
+    const [isLinkActive,setIsLinkActive] = useState('home');
+    const [scrolled,setScrolled] = useState(false);
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      }
+  
+      window.addEventListener("scroll", onScroll);
+  
+      return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+  
+    const onUpdateActiveLink = (value) => {
+      setIsLinkActive(value);
+    }
+
     return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+     <Navbar expand="md">
         <Container>
           <Navbar.Brand href="/">
-           
+            {/* <img src={ss_logo} alt="Logo" /> */}
           </Navbar.Brand>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={isLinkActive === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={isLinkActive === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={isLinkActive === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>
+              {/* <HashLink to='#connect'> */}
+                <button className="vvd"><span>Let’s Connect</span></button>
+              {/* </HashLink> */} 
+            </span>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
